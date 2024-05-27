38f155fe8c68c48df7be318692dfd7982ef91fec
Alexander <107767827+entgorbunov@users.noreply.github.com>
2024-05-27 20:21:12 +0500

    exceptions homework

---
+++ src/main/java/com/oop/ExceptionExample.java
@@ -0,0 +1,22 @@
+package com.oop;
+
+public class ExceptionExample {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0; // Эта строка вызовет ArithmeticException
+        } catch (ArithmeticException e) {
+            System.out.println("Произошла арифметическая ошибка: " + e.getMessage());
+        } finally {
+            System.out.println("Блок деления завершен.");
+        }
+
+        try {
+            int[] array = new int[5];
+            int number = array[10]; // Эта строка вызовет ArrayIndexOutOfBoundsException
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка доступа к массиву: " + e.getMessage());
+        } finally {
+            System.out.println("Блок доступа к массиву завершен.");
+        }
+    }
+}
